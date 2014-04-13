5bf8c3334b3e54cc17a779d95362413d243ba622
Jan-Henrik <jh.bruhn@me.com>
2014-04-13 14:13:49 +0200

    Using nodewebkit to start this thing

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,12 +11,13 @@ module.exports = function (grunt) {
         nodewebkit: {
             options: {
                 build_dir: './webkitbuilds', // Where the build version of my node-webkit app is saved
+                version: "0.8.5",
                 mac: true, // We want to build it for mac
                 win: true, // We want to build it for win
                 linux32: false, // We don't need linux32
                 linux64: false // We don't need linux64
             },
-            src: ['./css/**', './fonts/**', './images/**', './js/**', './node_modules/**', '!./node_modules/grunt*/**', './index.html', './package.json', './README.md' ]
+            src: ['./css/**', './fonts/**', './images/**', './js/**', './node_modules/**', '!./node_modules/grunt*/**', '!./node_modules/nodewebkit*/**', './index.html', './package.json', './README.md' ]
         }
     });
 
@@ -24,29 +25,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-node-webkit-builder');
 
-    // Default task.
-    grunt.registerTask('default', ['run']);
-    grunt.registerTask('run', ['nodewebkit', 'start-nw']);
-    
-    grunt.registerTask('start-nw', function () {
-        var filePath = grunt.config.get('nodewebkit').options.build_dir;
-        filePath = path.join(filePath, "releases", "keybase-gui");
-
-        if (os.platform() === 'win32') {
-            var executable = path.join(".", filePath, "win", "keybase-gui", "keybase-gui.exe");
-            console.log(executable);
-            grunt.util.spawn({ cmd: executable }, function (error, res, code) {
-                if(err) throw err;
-                console.log("42");
-            });   
-        } else if (os.platform() === 'osx') {
-            var executable = path.join(filePath, "mac", "keybase-gui.app");
-            grunt.util.spawn("open " + executable, function(error, res, code) {
-            }); 
-        } else if (os.platform() === 'linux') {
-            console.log("Would you kindly implement linux support?");
-        }
-    });
+    grunt.registerTask('build', ['nodewebkit']);
     
 
 };
