175da1e60c368021366e045eef637172bb85bdaa
Jan-Henrik Bruhn <jh.bruhn@me.com>
2014-05-03 11:59:35 +0200

    Better Build Number

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ if [ ${TRAVIS_JOB_NUMBER##*.} -eq 1 ] && [ "$TRAVIS_BRANCH" = "master" ]; then
 	echo "$TRAVIS_BUILD_NUMBER" > dist/version.txt
 	ls
   sudo pip install ghp-import
-	ghp-import -n dist/ -m "Deploy ${TRAVIS_BUILD_NUMBER}."
+	ghp-import -n dist/ -m "Deploy ${TRAVIS_BUILD_NUMBER%.*}."
 	git push -fq https://$GIT_TOKEN@github.com/$TRAVIS_REPO_SLUG.git gh-pages > /dev/null
 fi
