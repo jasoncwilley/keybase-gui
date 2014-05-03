3a1ac5b8a7e85643e4cafd9b67ce96bd18525f3b
Jan-Henrik Bruhn <jh.bruhn@me.com>
2014-05-03 10:27:55 +0200

    Added Buildscript, first try.

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+if [ ${TRAVIS_JOB_NUMBER##*.} -eq 1 ] && [ "$TRAVIS_BRANCH" = "master" ]; then
+	echo "$TRAVIS_BUILD_NUMBER" > dist/version.txt
+	sudo pip install ghp-import
+	ghp-import -n webkitbuilds/ -m "Deploy ${TRAVIS_BUILD_NUMBER}."
+	git push -fq https://$GIT_TOKEN@github.com/$TRAVIS_REPO_SLUG.git gh-pages > /dev/null
+fi
