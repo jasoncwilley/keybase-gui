57bce6f77e87aad980c6664df82bc7d35db8a109
Jan-Henrik Bruhn <jh.bruhn@me.com>
2014-05-03 12:03:05 +0200

    Again

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ if [ ${TRAVIS_JOB_NUMBER##*.} -eq 1 ] && [ "$TRAVIS_BRANCH" = "master" ]; then
   mkdir -p dist
   cd webkitbuilds/releases/keybase-gui
   for D in *; do
-    FILENAME=keybase-gui.$D.$TRAVIS_JOB_NUMBER.zip
+    FILENAME=keybase-gui.$D.$TRAVIS_BUILD_NUMBER.zip
     zip -r $FILENAME $D
     cp $FILENAME ../../../dist/
   done
@@ -13,6 +13,6 @@ if [ ${TRAVIS_JOB_NUMBER##*.} -eq 1 ] && [ "$TRAVIS_BRANCH" = "master" ]; then
 	echo "$TRAVIS_BUILD_NUMBER" > dist/version.txt
 	ls
   sudo pip install ghp-import
-	ghp-import -n dist/ -m "Deploy ${TRAVIS_BUILD_NUMBER%.*}."
+	ghp-import -n dist/ -m "Deploy ${TRAVIS_BUILD_NUMBER}."
 	git push -fq https://$GIT_TOKEN@github.com/$TRAVIS_REPO_SLUG.git gh-pages > /dev/null
 fi
