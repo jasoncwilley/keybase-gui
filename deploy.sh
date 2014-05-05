ab75a07f482e33483388fc7441201e63260ffb68
Jan-Henrik <jh.bruhn@me.com>
2014-05-05 22:02:45 +0200

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,11 @@ if [ ${TRAVIS_JOB_NUMBER##*.} -eq 1 ] && [ "$TRAVIS_BRANCH" = "master" ]; then
   mkdir -p dist
   cd webkitbuilds/releases/keybase-gui
   for D in *; do
-    FILENAME=keybase-gui.$D.$TRAVIS_BUILD_NUMBER.zip
-    zip -r $FILENAME $D/keybase-gui/
-    cp $FILENAME ../../../dist/
+    if [ -d "${D}" ]; then
+      FILENAME=keybase-gui.$D.$TRAVIS_BUILD_NUMBER.zip
+      zip -r $FILENAME $D/keybase-gui/
+      cp $FILENAME ../../../dist/
+    fi
   done
   cd ../../../
 	echo "$TRAVIS_BUILD_NUMBER" > dist/version.txt
